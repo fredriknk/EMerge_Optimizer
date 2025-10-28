fbdd9657ccdaa4b3e32e48ec2014b2dbd9355586
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2025-10-28 13:07:46 +0100

    debug print

--- optimize_lib.py
+++ optimize_lib.py
@@ -466,7 +466,9 @@ def _objective_factory(
             # Optional center weighting (very light)
             beta = 0.1  # set 0.0 to disable
             ex0 = float(max(_gamma_from_rl_db(np.array([rl_f0]))[0] - gam_target, 0.0))
+            
             frac_ok = float(np.mean(rl[m] >= rl_target))
+            print(f"frac_ok debug: rl_f0={rl_f0:.2f} dB, frac_ok={frac_ok:.2f}")
             obj = -1*(rl_f0+rl_f0*frac_ok)
 
             rl_min_band = float(np.min(rl[m]))
